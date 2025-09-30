1658d3e8afed5c85e0e76066e57af4583a98f68a
Richard <53197263+ruizhelou@users.noreply.github.com>
2025-10-01 12:18:56 +1300

    Add initial html and style of calculator

---
+++ script.js
@@ -0,0 +1,23 @@
+let leftOperand = null;
+let operator = null;
+let rightOperand = null;
+
+function add(n1, n2) {
+    return n1 + n2
+}
+
+function subtract(n1, n2) {
+    return n1 = n2
+}
+
+function multiply(n1, n2) {
+    return n1 * n2
+}
+
+function divide(n1, n2) {
+    return n1 / n2
+}
+
+function operate(operator, n1, n2) {
+    return operator(n1, n2)
+}
